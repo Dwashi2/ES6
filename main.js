21be2a0de1de130d1e12c52f476717f7befc46ad
Dwashi2 <67284604+Dwashi2@users.noreply.github.com>
2020-11-16 15:36:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,56 @@
-class Usuario {
-    constructor(email, password) {
-        this.email = email;
-        this.password = password;
-    }
-    isAdmin() {
-        return this.admin === true;
-    }
+// 3.1
+const arr = [1, 2, 3, 4, 5];
+arr.map(function (item) {
+    return item + 10;
+});
 
+//Arrow Function
+const arr = [1, 2, 3, 4, 5];
+
+const newArr = arr.map(() => item + 10)
+
+
+
+// 3.2
+const usuario = { nome: 'Diego', idade: 23 };
+function mostraIdade(usuario) {
+    return usuario.idade;
 }
-class Admin extends Usuario {
-    constructor() {
-        super();
-        this.admin = true;
-    }
+mostraIdade(usuario);
+
+
+//Arrow Function
+const usuario = { nome: 'Diego', idade: 23 };
+
+const mostraIdade = usuario => usuario.idade
+
+mostraIdade(usuario);
+
+// 3.3
+const nome = "Diego"; const idade = 23;
+function mostraUsuario(nome = 'Diego', idade = 18) {
+    return { nome, idade };
+}
+mostraUsuario(nome, idade);
+mostraUsuario(nome);
+
+
+//Arrow Function
+const nome = "Diego";
+const idade = 23;
+
+const mostraUsuario = (nome = 'Diego', idade = 18) => ({ nome, idade });
+
+mostraUsuario(nome, idade);
+mostraUsuario(nome);
+
+// 3.4
+const promise = function () {
+    return new Promise(function (resolve, reject) {
+        return resolve();
+    })
 }
 
-const User1 = new Usuario('email@teste.com', 'senha123');
-const Adm1 = new Admin('email@teste.com', 'senha123');
+//Arrow Function
 
-
-
-console.log(User1.isAdmin());
-console.log(Adm1.isAdmin()); 
+const promise = () => new Promise(resolve,reject => resolve())
